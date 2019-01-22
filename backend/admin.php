b4ed9efedcc8990c52170eb0e8dbe9f29c6d749c
Eli Kimchi <rabu8770@gmail.com>
2019-01-22 10:28:09 -0800

    set sam's function up

---
+++ backend/admin.php
@@ -0,0 +1,9 @@
+<?php
+    //require_once 'verify.php';
+    require_once 'connection.php';
+
+    if($_GET['func'] == 'load') {
+        $result = $db->query('SELECT id, first_name, last_name, email, grad_year FROM students');
+        echo json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+    }
+?>
--- backend/functions.php
+++ backend/functions.php
@@ -1,6 +1,3 @@
 <?php
 
-    // Printing JSON function
-    $result = $db->query('SELECT id, first_name, last_name, email, grad_year FROM students');
-    echo json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
 ?>
