5fc8c695aefba276a68009cbe8088fb18b04ff3a
eta57 <theeta57@gmail.com>
2019-01-24 15:08:42 -0800

    Verify and Admin.php work

--- backend/admin.php
+++ backend/admin.php
@@ -1,9 +1,13 @@
 <?php
-    //require_once 'verify.php';
+    require_once './verify.php';
     require_once './connection.php';
-
-    if($_GET['func'] == 'load') {
-        $result = $db->query('SELECT id, first_name, last_name, email, grad_year FROM students');
-        echo json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+    if($access == 1) {
+        if($_GET['func'] == 'load') {
+            $result = $db->query('SELECT id, first_name, last_name, email, grad_year FROM students');
+            echo json_encode($result->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+        }
+    } else {
+        die();
     }
+    
 ?>
--- backend/verify.php
+++ backend/verify.php
@@ -1,10 +1,12 @@
 <?php
+
 $key = $_COOKIE["login_key"];
-if(db->query("SELECT id FROM `students` WHERE `login_key` = ".$key.";")->num_rows){
-    echo "2";
-}elseif(db->query("SELECT id FROM `admin` WHERE `login_key` = ".$key)->num_rows){
-    echo "1";
+if($db->query("SELECT id FROM `students` WHERE `login_key` = ".$key)->num_rows){
+    $access = 2;
+}elseif($db->query("SELECT id FROM `admin` WHERE `login_key` = ".$key)->num_rows){
+    $access = 1;
 }else{
-    echo "0";
+    setcookie('login_key', '', -100, '/');
+    die();
 }
 ?>
