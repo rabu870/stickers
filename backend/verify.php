5925297ef1072d64a3c64229c3e561ed7143981c
eta57 <theeta57@gmail.com>
2019-01-24 14:20:02 -0800

    Verify Start
    My DB connection isn't working so I can't test, but it looks good to me.

--- backend/verify.php
+++ backend/verify.php
@@ -1,3 +1,10 @@
 <?php
-
+$key = $_COOKIE["login_key"];
+if(db->query("SELECT id FROM `students` WHERE `login_key` = ".$key)->num_rows()){
+    echo "2";
+}elseif(db->query("SELECT id FROM `admin` WHERE `login_key` = ".$key)->num_rows()){
+    echo "1";
+}else{
+    echo "0";
+}
 ?>
