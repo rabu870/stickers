94e5f760e62f6126e359b64e0712e29a650e8bb9
eta57 <theeta57@gmail.com>
2019-01-24 14:48:07 -0800

    Verify trash

--- backend/verify.php
+++ backend/verify.php
@@ -1,8 +1,8 @@
 <?php
 $key = $_COOKIE["login_key"];
-if(db->query("SELECT id FROM `students` WHERE `login_key` = ".$key)->num_rows()){
+if(db->query("SELECT id FROM `students` WHERE `login_key` = ".$key.";")->num_rows){
     echo "2";
-}elseif(db->query("SELECT id FROM `admin` WHERE `login_key` = ".$key)->num_rows()){
+}elseif(db->query("SELECT id FROM `admin` WHERE `login_key` = ".$key)->num_rows){
     echo "1";
 }else{
     echo "0";
