3ece4ae441f65f12bad709233069643ec1b10f3e
eta57 <theeta57@gmail.com>
2019-01-25 11:42:10 -0800

    Made verify more efficient

--- backend/verify.php
+++ backend/verify.php
@@ -8,9 +8,7 @@ if($db->query("SELECT id FROM `students` WHERE `login_key` = '$key'")->num_rows)
 } elseif($db->query("SELECT id FROM `admin` WHERE `login_key` = '$key'")->num_rows) {
     $access = 1;
 } else {
-    setcookie('login_key', '', -100, '/');
-    echo 0;
-    die();
+    $access = 0;
 }
 if($_GET['client']) {
     echo $access;
