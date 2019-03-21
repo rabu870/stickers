e48c4feb46d239716ffef0d39ec86c1870715fdc
Samuel Boerner <samuel.boerner@pscs.org>
2019-03-21 12:24:36 -0700

    Redirects to login if user not logged in

--- assets/scripts/conflicts.js
+++ assets/scripts/conflicts.js
@@ -1,6 +1,19 @@
 var vm = new Vue({
-  el: "#conflicts-page",
-  data: {
-    welcome: "Welcome to PSCS Conflicts!"
-  }
+    el: "#conflicts-page",
+    data: {
+        welcome: "Welcome to PSCS Conflicts!"
+    },
+    methods: {
+        verify: function () {
+            axios.get('./backend/verify.php?client=true')
+            .then(function (response) {
+                if (response.data == '0' || response.data == '1') {
+                    window.location.href = 'login/';
+                }
+            });
+        },
+    },
+    beforeMount() {
+        this.verify();
+    }
 });
