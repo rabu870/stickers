eefe5802a98b5ece9a257a712953a7b3db2de01c
Jacknote <jack.notarangelo@pscs.org>
2019-03-21 11:49:05 -0700

    starting to work on the conflicts.php backend file

--- backend/conflicts.php
+++ backend/conflicts.php
@@ -2,4 +2,16 @@
 require_once './connection.php';
 require_once './functions.php';
 
-$classes = $db->query("SELECT * FROM classes WHERE true");
+$classes = $db->query("SELECT class_name, id FROM classes WHERE true")->fetch_all();
+
+
+for($i=0; $i < count($classes); $i++){
+    print_r($classes[$i][0]);
+    echo("<br>");
+}
+
+json_encode($classes, JSON_PRETTY_PRINT);
+
+/*
+$classes = json_decode($classes);
+echo($classes);*/
