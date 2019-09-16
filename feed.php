e1d98856e38cb21baac91aaeda99bb2aa0a6bd0f
Eli Kimchi <rabu8770@gmail.com>
2019-09-15 17:27:07 -0700

    fix

--- feed.php
+++ feed.php
@@ -23,32 +23,6 @@
     
     function printClass($class, $class_info, $class_stickers) {
         global $students;
-        
-        // $html = "<div class='class-display card' style='border: none;'><div class='card-header'>";
-        // $html .= "<div class='class-name card-title h1'>" . $class_info['class_name'] . '</div>';
-        // $html .= "<div class='class-facil card-subtitle h3'>" . $class_info['facilitator'] . '</div>';
-        // $html .= $class_info['availability'] !== '' ? "<div class='class-availabilty card-subtitle h5'>" . $class_info['availability'] . '</div>' : '';
-        // $html .= $class_info['needs'] !== '' ? "<div class='class-needs card-subtitle h5'>" . $class_info['needs'] . '</div><div>' : '<div>';
-        // $html .= $class_info['tags'] == 'hs-only' ? "<span class='label label-primary' style='font-size:1rem'>HS Only</span>" : "";
-        // $html .= $class_info['tags'] == 'ms-only' ? "<span class='label label-primary' style='font-size:1rem'>MS Only</span>" : "";
-        // $html .= $class_info['is_mega'] == '1' ? "<span class='label label-primary' style='font-size:1rem'>Mega</span>" : "";
-        // $html .= $class_info['is_block'] == '1' ? "<span class='label label-primary' style='font-size:1rem'>Block</span>" : "";
-        // $html .= $class_info['mature'] == '1' ? "<span class='label label-primary' style='font-size:1rem'>Mature themes</span>" : "";
-        // $html .= "</div></div>";
-        // $html .= "<div class='card-body columns'>";
-        // $html .= "<div class='sticker-column column'>";
-        // $previouspriority = 1;
-        // foreach($class_stickers as $sticker){
-        //     while($sticker['priority'] > $previouspriority){
-        //         $html .= "</div><div class='sticker-column column'>";
-        //         $previouspriority++;
-        //     }
-        //     $html .= "<span class='sticker label menu'>" . $students[$sticker['student_id']]['first_name'] . " " . $students[$sticker['student_id']]['last_name'][0] . ".</span>";
-        // }while($previouspriority < 3){
-        //     $html .= "</div><div class='sticker-column column'>";
-        //     $previouspriority++;
-        // }
-        // $html .= "</div></div></div>";
 
         $html = '{';
         $html .= '"className": "' . $class_info['class_name'] . '",';
@@ -57,15 +31,19 @@
         $html .= '"isBlock": "' . $class_info['is_block'] . '",';
         $html .= '"availability": "' . $class_info['availability'] . '",';
         $html .= '"stickers": [';
-        foreach($class_stickers as $key=>$sticker) {
+        for($i = 0; $i < count($class_stickers); $i++) {
+            $sticker = array_values($class_stickers)[$i];
             $html .= '{"student": "' . $students[$sticker['student_id']]['first_name'] . " " . $students[$sticker['student_id']]['last_name'] . '",';
             $html .= '"priority": "' . $sticker['priority'] . '"';
             $html .= '}';
+            if($i + 1 <count($class_stickers)) {
+                $html .= ',';
+            }
+            
         }
         $html .= ']}';
 
         print_r($html);
-        //print_r($class_stickers);
     }
 
 ?>
@@ -86,10 +64,6 @@
                     }
                 }
                 echo ']';
-            }//else{
-            //     echo '[';
-            //     printClass($_GET['class'],$classes[0],$stickers);
-            //     echo ']';
-            // }
+            }
         ?>
         </pre>
