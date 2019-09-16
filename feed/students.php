d1ef320001849dcbdc15f882da6e6e6cf6f23e68
Eli Kimchi <rabu8770@gmail.com>
2019-09-16 13:20:00 -0700

    remove unneccesary code

--- feed/students.php
+++ feed/students.php
@@ -2,15 +2,7 @@
 
     require_once '../backend/connection.php';
 
-    $students = [];
-    $students_indexed = [];
-
     $students = $db->query('SELECT `first_name`, `last_name`, `id`, `email`, `advisor` FROM `students` WHERE true')->fetch_all($resulttype = MYSQLI_ASSOC);
-    foreach($students as $student){
-        $students_indexed[$student['id']] = $student;
-    }
-
-    $students = $students_indexed;
 
     echo "[";
     foreach($students as $key=>$student) {
@@ -24,18 +16,4 @@
         }
     }
     echo "]";
-
-    if(empty($_GET['class'])){
-        echo '[';
-        foreach($classes as $key=>$c) {
-            printClass($c['id'], $c, array_filter($stickers, function ($s) {
-                global $c;
-                return $s['class_id'] == $c['id'];
-            }));
-            if($key + 1 <count($classes)) {
-                echo ',';
-            }
-        }
-        echo ']';
-    }
 ?>
